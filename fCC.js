a70b9af1b12e096ba868e7286b2d3b399caaa6b1
ynnabaina <ynnasalzw@gmail.com>
2021-05-31 17:15:45 -0700

    challenge 2

---
+++ fCC.js
@@ -0,0 +1,9 @@
+// Challenge 1
+// Challenge is to do an inline comment and
+/* block 
+comment */
+// ==================== COMPLETE
+
+// Challenge 2
+//  Declare JavaScript VariablesPassed
+// var myName;
