91edb1cd64562b03a864ae0753423e8d80861e4f
ynnabaina <ynnasalzw@gmail.com>
2021-06-04 17:27:36 -0700

    day 2, missed a few days but will try better

--- fCC.js
+++ fCC.js
@@ -18,7 +18,7 @@ a = 7;
 // ==================== COMPLETE
 
 // challenge 4
-// assigning the calue of one variable to another
+// assigning the value of one variable to another
 /* 
 var a; 
 a = 7
@@ -34,3 +34,102 @@ b = a;
 var sum = 10 + 10 
 */
 // ==================== COMPLETE
+
+// challenge 6
+// Understanding Case Sensitivity in Variables
+
+//---- declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+//---- assignments
+studlyCapVar = 10;
+properCamelCase = "a String";
+titleCaseOver = 9000;
+
+// challenge 7
+// Understanding Uninitialized Variables
+
+var a = 5;
+var b = 10;
+var c = "I am a ";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// challenge 8
+// Initializing Variables with the Assignment Operator
+
+var myVar = 9;
+
+// challenge 9
+// Subtract One Number from Another with JavaScript
+// T: Change the 0 so the difference is 12.
+var difference = 45 - 33;
+
+// challenge 10
+// Multiply Two Numbers with JavaScript
+// T: Change the 0 so that product will equal 80
+var product = 8 * 10;
+
+// challenge 11
+// Divide One Number by Another with JavaScript
+//T: Change the 0 so that the quotient is equal to 2.
+var quotient = 66 / 33;
+
+// challenge 12
+// Increment a number with javascript
+// T: change the code to use the ++ operator on myVar
+var myVar = 87;
+
+myVar++;
+
+// challenge 13
+// Decrement a number with javascript
+// T: change the code to use the -- operator on myVar
+var myVar = 11;
+
+myVar--;
+
+//challenge 14
+// create decimal numbers with javascript
+// T: myDecimal should be a number
+// T: myDecimal should have a decimal point
+var ourDecimal = 5.7;
+
+var myDecimal = 9.6;
+
+//challenge 15
+//Multiply two decimals with javascript
+//T: change the 0.0 so that product will equal 5.0
+//T: you should use the * operator
+var product = 5.0 * 1.0;
+
+// challenge 16
+// divide one decimal by another with javascript
+//T: the variable quotient should equal 2.2
+//T: you should use hte / operator to divide 4.4 2
+//T: the quotient variable should only be assigned once
+var quotient = 4.4 / 2;
+
+//challenge 17
+// finding a remainder in javascript
+//T: Set remainder equal to the remainder of 11 divided by 3 using the remainder % operator
+var remainder = 11 % 3;
+
+// challenge 18
+// Compound Assignment With Augmented Addition
+// T: Convert the assignments for a, b, and c to use the += operator.
+var a = 3;
+var b = 9;
+var c = 12;
+a += 12;
+b += 9;
+c += 7;
+
+// challenge 19
+// Compound Assignment With Augmented Subtraction
+// T: a should equal 5.
+// T:
